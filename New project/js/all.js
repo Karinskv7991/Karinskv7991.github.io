290e2e6dea734a2ba989de9cbfb9aaa451b1722a
Karina <karinaskirtach@gmail.com>
2020-07-08 01:26:05 +0200

    added New project

---
+++ New project/js/all.js
@@ -0,0 +1,17 @@
+			let burger = document.getElementById("burger");
+      let nav = document.getElementById("nav");
+      let navLink = document.getElementsByClassName("nav__link");
+      console.log(navLink);
+
+      burger.addEventListener("click", menu);
+      function menu() {
+        nav.classList.toggle("nav_hidden");
+        burger.classList.toggle("close");
+      }
+
+      for (let i = 0; i < navLink.length; i++) {
+        navLink[i].addEventListener("click", menuClose);
+      }
+      function menuClose() {
+        nav.classList.add("nav_hidden");
+      }
