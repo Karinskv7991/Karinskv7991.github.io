335276126f7e253d680e44dfab494fc5283f7fc5
Karina <karinaskirtach@gmail.com>
2020-07-09 02:14:28 +0200

    slick unstyled

--- New project/js/all.js
+++ New project/js/all.js
@@ -1,3 +1,5 @@
+			// бургер
+
 			let burger = document.getElementById("burger");
       let nav = document.getElementById("nav");
       let navLink = document.getElementsByClassName("nav__link");
@@ -16,3 +18,21 @@
         nav.classList.remove("nav_active");
 				burger.classList.remove("close");
       }
+
+			// слайдер для 2 секции
+
+			$('.container').slick();
+			// ниже, чтобы слайдер появлялся на экранах, разрешение которых ниже 768px
+			$(window).on('load resize', function() {
+  if ($(window).width() < 768) {
+    $('.container:not(.slick-initialized)').slick({
+      centerMode: true,
+      dots: true,
+      infinite: true,
+      speed: 100,
+      slidesToShow: 1
+    });
+  } else {
+    $(".container.slick-initialized").slick("unslick");
+  }
+});
