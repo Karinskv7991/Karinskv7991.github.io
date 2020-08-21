7aa0806ff8ca32ed4a4a7cd68a6cb65bd66d63ca
Karina <karinaskirtach@gmail.com>
2020-08-21 17:07:50 +0200

    'first'

---
+++ thrive/js/main.js
@@ -0,0 +1,20 @@
+// бургер
+
+let burger = document.getElementById("burger");
+let nav = document.getElementById("nav");
+let navLink = document.getElementsByClassName("nav__link");
+console.log(navLink);
+
+burger.addEventListener("click", menu);
+function menu() {
+	nav.classList.toggle("nav_active");
+	burger.classList.toggle("close");
+}
+
+for (let i = 0; i < navLink.length; i++) {
+	navLink[i].addEventListener("click", menuClose);
+}
+function menuClose() {
+	nav.classList.remove("nav_active");
+	burger.classList.remove("close");
+}
